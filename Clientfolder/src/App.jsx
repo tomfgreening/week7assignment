8e8845da47f01c82c2661951083e61616d757b16
Thomas Greening <tomfgreening@gmail.com>
2025-02-01 12:48:15 +0000

    Scoped down some routes. Begining debugging of server and client form interactions.

--- Clientfolder/src/App.jsx
+++ Clientfolder/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -15,10 +15,10 @@ export default function App() {
         <Route element={<About />} path={"/about"} />
         <Route element={<Contact />} path={"/contact"} />
       </Routes>
-      <Form />
       <div>
-        <h1>Hello, you!</h1>
+        <h3>We hope you enjoyed your visit to Juicy Jake's vegan returant! Please use the form below to sign our guestbook.</h3>
       </div>
+      <Form />
       <Footer />
     </>
   );
--- Serverfolder/server.js
+++ Serverfolder/server.js
@@ -6,28 +6,25 @@ app.use(express.json());
 app.use(cors());
 
 app.listen(8080, () => {
-    console.log("Server is running on local port 8080!");
+  console.log("Server is running on local port 8080!");
 });
 
 app.get("/", function (req, res) {
-    res.json ({message:"this is the root route."});
+  res.json({ message: "this is the root route." });
 });
 
 app.get("/readroute", async (req, res) => {
-    const result = await db.query("SELECT * FROM {JJguestbook}");
-    res.json(result.rows);
+  const result = await db.query("SELECT * FROM jjguestbook");
+  res.json(result.rows);
 });
 
-app.post("/createtableEntry", (req, res) => {
-    const newData = req.body;
-    const query = db.query (
-        'INSERT INTO {JJguestbook} (name, date_of_visit, comments) VALUES ($1, $2, $3) RETURNING *',
-        [
-            newData.guestName,
-            newData.date_of_visit,
-            newData.comments,
-    ]
-    );
-    res.json({ message: "Data sent to the database!" });
-    //   res.json(query.rows);
+app.post("/createtableEntry", async (req, res) => {
+  console.log("Received data:", req.body);
+//   line 23 is a debugging step to see if the data is actually being sent, it is! I can see the line 22 message in the server terminal. 
+  const newData = req.body;
+  const query = await db.query(
+    "INSERT INTO jjguestbook (name, date_of_visit, comments) VALUES ($1, $2, $3) RETURNING *",
+    [newData.guestName, newData.date_of_visit, newData.comments]
+  );
+  res.json(query.rows);
 });
