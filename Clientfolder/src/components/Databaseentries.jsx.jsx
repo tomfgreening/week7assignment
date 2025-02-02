902942a09d94492128686a6101b3c098aebe10ce
Thomas Greening <tomfgreening@gmail.com>
2025-02-02 19:06:20 +0000

    Cleaned up databaseentries file.

--- Clientfolder/src/components/Databaseentries.jsx.jsx
+++ Clientfolder/src/components/Databaseentries.jsx.jsx
@@ -2,49 +2,30 @@ import { useState, useEffect } from "react";
 export default function DatabaseEntries() {
   const [entries, setEntries] = useState([]);
   useEffect(() => {
-    async function fetchEntries () {
-        try {
-            const response = await fetch ("http://localhost:8080/readroute");
-            const data = await response.json();
-            setEntries(data);
-        } catch (error) {
-            console.error("Unable to fetch entries:", error);
-        }
+    async function fetchEntries() {
+      try {
+        const response = await fetch("http://localhost:8080/readroute");
+        const data = await response.json();
+        setEntries(data);
+      } catch (error) {
+        console.error("Unable to fetch entries:", error);
+      }
     }
     fetchEntries();
-}, []);
-}
+  }, []);
 
-return (
+  return (
     <div>
-        <h1>Guestbook Entires</h1>
-        <ul>
-            {entires.map((entry)=>(
-                <li key={entry.id}</li>
-                <li name={entry.name}</li>
-                <li date_of_visit={entry.date_of_visit}
-                <li comments={entry.comments}
-            ) )}
-        </ul>
+      <h1>Guestbook Entires</h1>
+      <ul>
+        {entries.map((entry) => (
+          <li key={entry.id}>
+            <p>name={entry.name} </p>
+            <p>date_of_visit={entry.date_of_visit} </p>
+            <p>comments={entry.comments} </p>
+          </li>
+        ))}
+      </ul>
     </div>
-)
-
-
-
-// return (
-//     <div>
-//       <h1>useEffect Demo!!!</h1>
-//       <p>{message}</p>
-//       <button onClick={() => setRefreshCount(refreshCount + 1)}>
-//         Refetch Data ({refreshCount})
-//       </button>
-//       <button onClick={() => setMessage("")}>Clear Message</button>
-//       <ul>
-//         {items.map((item) => (
-//           <li key={item.id}>{item.title}</li>
-//           //every item that was fetched is going to be rendered as a list item element.
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
+  );
+}
