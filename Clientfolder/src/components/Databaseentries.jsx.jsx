84f383d63e88c951bef890f9eb02f012f4885ac2
Thomas Greening <tomfgreening@gmail.com>
2025-02-02 18:30:12 +0000

    Pasted instructor code in to guide building the returning section.

--- Clientfolder/src/components/Databaseentries.jsx.jsx
+++ Clientfolder/src/components/Databaseentries.jsx.jsx
@@ -13,34 +13,31 @@ export default function DatabaseEntries() {
     }
     fetchEntries();
 }, []);
-  });
 }
 
-// useEffect(() => {
-//     async function fetchData() {
-//       try {
-//         const response = await fetch(
-//           "https://jsonplaceholder.typicode.com/todos"
-//           //fetches a list of todos from the API
-//         );
-//         const data = await response.json();
-//         // JSON wrangle!
+return (
+    <div>
+        <h1>Guestbook Entires</h1>
+        <ul></ul>
+    </div>
+)
 
-//         console.log("EFFECT!!!: Data fetched successfully!");
-//         //console log then data is fetched
 
-//         setItems(data);
-//         setMessage(
-//           `SIDE EFFECT!!!: Fetched ${data.length} items successfully!`
-//         );
-//       } catch (error) {
-//         console.error("SIDE EFFECT!!!: Failed to fetch items!");
-//         setMessage("SIDE EFFECT!!!: Failed to fetch items!");
-//         // If our fetch doesnt work then we get a message in our console (a side effect) and we set the value of the message variable in state to the error message using the setMessage mutation function
-//       }
-//     }
-//     fetchData();
-//     // we need a dpendency array (its written between square brackets at the end of our useEffect and it can be empty!)
-//     //dependency array tells useEffect when to run and in what way to run
-//     // an empty dependency array will have useEffect run its function only one, on reder of the page/component.
-//   }, [refreshCount]);
+
+// return (
+//     <div>
+//       <h1>useEffect Demo!!!</h1>
+//       <p>{message}</p>
+//       <button onClick={() => setRefreshCount(refreshCount + 1)}>
+//         Refetch Data ({refreshCount})
+//       </button>
+//       <button onClick={() => setMessage("")}>Clear Message</button>
+//       <ul>
+//         {items.map((item) => (
+//           <li key={item.id}>{item.title}</li>
+//           //every item that was fetched is going to be rendered as a list item element.
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
